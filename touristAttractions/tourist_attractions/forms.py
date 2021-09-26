0fec44254c080ac4ed1130bfb86a0875b62f0002
Klinsmann-Agyei <87317341+Klinsmann-Agyei@users.noreply.github.com>
2021-09-26 08:33:30 +0000

    Tourist Attraction with forms project

---
+++ touristAttractions/tourist_attractions/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.db.models import fields
+from .models import State, Attraction
+
+class StateCreateForm(forms.ModelForm):
+    class Meta:
+        model = State
+        fields = "__all__"
+
+class AttractionCreateForm(forms.ModelForm):
+    class Meta:
+        model = Attraction
+        fields = "__all__"
+        
+
+
